39f05127a16c66cff8ca4305417bd13cbea56893
VadymGor <07tennis@gmail.com>
2020-04-25 20:24:23 +0200

    View Controller Lifecycle and Scroll View

---
+++ Cassini/Cassini/ImageViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Vadym Horovyi on 25/04/2020.
+//  Copyright © 2020 Vadym Horovyi. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    var imageURL: URL? {
+        didSet {
+            imageView.image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
+    }
+}
