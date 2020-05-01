cb0ddd6be410c2e631d631783d611e2acf9ea522
VadymGor <07tennis@gmail.com>
2020-05-01 15:17:59 +0200

    scroll view from code

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -12,10 +12,21 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     
     var imageURL: URL? {
         didSet {
-            imageView.image = nil
+            image = nil
             if view.window != nil {     // we're on Screen!!
                 fetchImage()
             }
+        }
+    }
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
         }
     }
     
@@ -26,14 +37,26 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
     
-    @IBOutlet weak var imageView: UIImageView!
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    var imageView = UIImageView()
     
     private func fetchImage() {
         if let url = imageURL {
             let urlContents = try? Data(contentsOf: url)
             if let imageData = urlContents {
-                imageView.image = UIImage(data: imageData)
+                image = UIImage(data: imageData)
             }
         }
     }
@@ -44,64 +67,4 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
             imageURL = DemoURLs.stanford
         }
     }
-    
 }
-
-//    var imageURL: URL? {
-//        didSet {
-//            image = nil
-//            if view.window != nil {     // we're on Screen!!
-//                fetchImage()
-//            }
-//        }
-//    }
-//
-//    private var image: UIImage? {
-//            get {
-//                return imageView.image
-//            }
-//            set {
-//                imageView.image = newValue
-//                imageView.sizeToFit()
-//                scrollView.contentSize = imageView.frame.size
-//            }
-//        }
-//
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        if imageView.image == nil {
-//            fetchImage()
-//        }
-//    }
-//
-//    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-//              return imageView
-//          }
-//
-//     @IBOutlet weak var scrollView: UIScrollView! {
-//            didSet {
-//                scrollView.minimumZoomScale = 1/25
-//                scrollView.maximumZoomScale = 1.0
-//                scrollView.delegate = self
-//                scrollView.addSubview(imageView)
-//            }
-//        }
-//
-//        var imageView = UIImageView()
-//
-//    private func fetchImage() {
-//        if let url = imageURL {
-//            let urlContents = try? Data(contentsOf: url)
-//            if let imageData = urlContents {
-//                image = UIImage(data: imageData)
-//            }
-//        }
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        if imageURL == nil {
-//            imageURL = DemoURLs.stanford
-//        }
-//    }
-//}
