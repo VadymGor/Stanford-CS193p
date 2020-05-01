d855ec98e883b00d678f68b739a6f1c6836ad997
VadymGor <07tennis@gmail.com>
2020-05-01 19:01:47 +0200

    Multithreading, Queues, async, sync

---
+++ Cassini/Cassini/CassiniViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Vadym Horovyi on 01/05/2020.
+//  Copyright © 2020 Vadym Horovyi. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+    
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if let url = DemoURLs.NASA[identifier] {
+                if let imageVC = segue.destination.contents as? ImageViewController {
+                    imageVC.imageURL = url
+                    imageVC.title = (sender as? UIButton)?.currentTitle
+                }
+            }
+        }
+    }
+}
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
+}
--- Cassini/Cassini/DemoURLs.swift
+++ Cassini/Cassini/DemoURLs.swift
@@ -10,15 +10,22 @@ import Foundation
 
 struct DemoURLs
 {
-	static let stanford = Bundle.main.url(forResource: "oval", withExtension: "jpg")
-	
-//    static let stanford = URL(string: "https://upload.wikimedia.org/wikipedia/commons/5/55/Stanford_Oval_September_2013_panorama.jpg")
-	
+    static let stanford = Bundle.main.url(forResource: "oval", withExtension: "jpg")
+    
+    //    static let stanford = URL(string: "https://upload.wikimedia.org/wikipedia/commons/5/55/Stanford_Oval_September_2013_panorama.jpg")
+    
     static var NASA: Dictionary<String,URL> = {
         let NASAURLStrings = [
-            "Cassini" : "https://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg",
-            "Earth" : "https://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg",
-            "Saturn" : "https://www.nasa.gov/sites/default/files/saturn_collage.jpg"
+            
+            //            another pictures for faster loading
+            "Cassini" : "https://www.nasa.gov/sites/default/files/thumbnails/image/cassinifinale.jpg",
+            "Earth" : "https://miro.medium.com/max/3240/1*y-93nelRzAVTMg2n7dpNlg.jpeg",
+            "Saturn" : "https://www.nasa.gov/sites/default/files/thumbnails/image/cassini_extrasolar_dust_main.jpg"
+            
+            
+//            "Cassini" : "https://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg",
+//            "Earth" : "https://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg",
+//            "Saturn" : "https://www.nasa.gov/sites/default/files/saturn_collage.jpg"
         ]
         var urls = Dictionary<String,URL>()
         for (key, value) in NASAURLStrings {
